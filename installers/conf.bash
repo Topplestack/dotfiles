8b24820fcfdb4275ef7930a87fcb589e29d4c97e
Jesse Taylor <jesset@bestfriends.org>
2021-12-23 11:58:14 -0700

    add zoom dependencies

--- installers/conf.bash
+++ installers/conf.bash
@@ -28,6 +28,7 @@ while true; do
 read -p "Do you want to install Zoom? y/n: " yn
     case $yn in
         [Yy]* )
+            sudo apt install libgl1-mesa-glx libegl1-mesa libxcb-xtest0
             wget https://zoom.us/client/latest/zoom_amd64.deb
             sudo dpkg -i zoom_amd64.deb
             rm zoom_amd64.deb
